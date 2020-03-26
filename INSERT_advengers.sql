47472a8fd71756f824ba03c97f2ec265a09f3f0c
ruolin97 <r6u2b@ugrad.cs.ubc.ca>
2020-03-26 16:55:11 -0700

    separate scripts

---
+++ INSERT_advengers.sql
@@ -0,0 +1,18 @@
+INSERT INTO Follows VALUES ('ironman', 'ppotts');
+INSERT INTO Follows VALUES ('spidey', 'ironman');
+INSERT INTO Follows VALUES ('blackwidow', 'hawkeye');
+INSERT INTO Follows VALUES ('hawkeye', 'blackwidow');
+INSERT INTO Follows VALUES ('ironman', 'spidey');
+
+INSERT INTO Plans VALUES ('ironman', 1, '2020-05-02');
+INSERT INTO Plans VALUES ('spidey', 2, '2020-06-01');
+INSERT INTO Plans VALUES ('blackwidow', 3, '2020-04-24');
+INSERT INTO Plans VALUES ('hawkeye', 4, '2020-04-24');
+INSERT INTO Plans VALUES ('ppotts', 5, '2020-04-04');
+
+INSERT INTO Takes VALUES (1, 'plane', 7000);
+INSERT INTO Takes VALUES (2, 'plane', 6000);
+INSERT INTO Takes VALUES (3, 'boat', 1000);
+INSERT INTO Takes VALUES (4, 'plane', 11000);
+INSERT INTO Takes VALUES (5, 'car', 800);
+
