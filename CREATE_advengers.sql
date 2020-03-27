39c1746fb55963fe165e7782514b4d8386d289f5
MagY <yangm110615@gmail.com>
2020-03-26 22:16:06 -0700

    changed create

--- CREATE_advengers.sql
+++ CREATE_advengers.sql
@@ -1,96 +1,68 @@
 -- TABLES --
 
 
-CREATE TABLE Users(
-	username CHAR(16) PRIMARY KEY,
-	password CHAR(30));
-CREATE TABLE Regular_User_Rankings(
-	date_joined DATE PRIMARY KEY,
-	rank INTEGER);
+CREATE TABLE All_Users(
+	username VARCHAR(16) PRIMARY KEY,
+	password VARCHAR(30));
 CREATE TABLE Regular_User(
-	username CHAR(16) PRIMARY KEY,
-	date_joined DATE,
-	profile_picture CHAR(70),
-	FOREIGN KEY (username) REFERENCES Users(username));
+	username VARCHAR(16) PRIMARY KEY,
+	profile_picture VARCHAR(70),
+	FOREIGN KEY (username) REFERENCES All_Users(username));
 CREATE TABLE Admin(
-	username CHAR(16) PRIMARY KEY,
-	name CHAR(70),
-	role CHAR(70),
-	email CHAR(70) UNIQUE NOT NULL,
-    	admin_ID INTEGER UNIQUE NOT NULL,
-	FOREIGN KEY (username) REFERENCES Users(username));
-CREATE TABLE Follows(
-	follower_username CHAR(16),
-	following_username CHAR(16),
-	PRIMARY KEY (follower_username, following_username),
-	FOREIGN KEY (follower_username) REFERENCES Users(username),
-	FOREIGN KEY (following_username) REFERENCES Users(username));
+	username VARCHAR(16) PRIMARY KEY,
+	name VARCHAR(70),
+	role VARCHAR(70),
+	email VARCHAR(70) UNIQUE NOT NULL,
+    admin_ID INTEGER UNIQUE NOT NULL,
+	FOREIGN KEY (username) REFERENCES All_Users(username));
 
 
-CREATE TABLE Distance(
-    	transportation CHAR(70),
-	km REAL,
-	PRIMARY KEY (transportation, km));
 CREATE TABLE Activity(
-	name CHAR(70) PRIMARY KEY,
-	type CHAR(70),
-	num_dollar_signs INTEGER,
-	description CHAR(120));
-CREATE TABLE Timezones(
-	country  CHAR(70),
-	province  CHAR(70),
-	time_zone  CHAR(5),
-	PRIMARY KEY (country, province));
+	name VARCHAR(70) PRIMARY KEY,
+	type VARCHAR(70),
+	num_dollar_signs INTEGER CHECK (num_dollar_signs IN (0, 1, 2, 3)),
+	description VARCHAR(120));
 CREATE TABLE Location(
 	id  INTEGER	PRIMARY KEY,
-	country  CHAR(70) NOT NULL,
-	province  CHAR(70) NOT NULL,
-    	city  CHAR(70) NOT NULL,
-postal_code  CHAR(6),
-address  CHAR(70) NOT NULL,
-UNIQUE (country, province, city, address));
-CREATE TABLE Trip_In
-	(title CHAR(70),
-    	trip_id INTEGER PRIMARY KEY,
-    	location_id INTEGER NOT NULL,
-    	hotel CHAR(50),
-   	duration INTEGER,
-    	description CHAR(120),
-    	FOREIGN KEY (location_id) REFERENCES Location(id));
+	country  VARCHAR(70) NOT NULL,
+	province  VARCHAR(70) NOT NULL,
+	city  VARCHAR(70) NOT NULL,
+	postal_code  VARCHAR(6),
+	address  VARCHAR(70) NOT NULL,
+	UNIQUE (country, province, city, address));
+CREATE TABLE Trip_In(
+	title VARCHAR(70),
+	trip_id INTEGER PRIMARY KEY,
+	location_id INTEGER NOT NULL,
+	transportation VARCHAR(50),
+   	duration VARCHAR(15) CHECK (duration IN ('daytrip', '1 week trip', '2 weeks+ trip')),
+	description VARCHAR(120),
+	FOREIGN KEY (location_id) REFERENCES Location(id));
 CREATE TABLE Restaurant(
-	name  CHAR(70)  PRIMARY KEY,
-	cuisine_type  CHAR(70),
-    	num_dollar_signs  INTEGER);
+	name  VARCHAR(70)  PRIMARY KEY,
+	cuisine_type  VARCHAR(70),
+	num_dollar_signs  INTEGER CHECK (num_dollar_signs IN (1, 2, 3)));
 
 
-CREATE TABLE Media
-	(title CHAR(70),
+CREATE TABLE Media(
+	title VARCHAR(70),
 	post_id INTEGER PRIMARY KEY,
-	description CHAR(120));
-CREATE TABLE Photo
-	(post_id INTEGER PRIMARY KEY,
-	file_path CHAR(70),
-    	FOREIGN KEY (post_id) REFERENCES Media(post_id));
-CREATE TABLE Video
-	(post_id INTEGER PRIMARY KEY,
-	url CHAR(70),
-    	FOREIGN KEY (post_id) REFERENCES Media(post_id));
+	description VARCHAR(120));
+CREATE TABLE Photo(
+	post_id INTEGER PRIMARY KEY,
+	file_path VARCHAR(70),
+	FOREIGN KEY (post_id) REFERENCES Media(post_id));
+CREATE TABLE Video(
+	post_id INTEGER PRIMARY KEY,
+	url VARCHAR(70),
+	FOREIGN KEY (post_id) REFERENCES Media(post_id));
 CREATE TABLE Text(
 	post_id INTEGER PRIMARY KEY,
-	words CHAR(120),
-	word_count INTEGER,
-    	language CHAR(30),
+	words VARCHAR(120),
+	language VARCHAR(30),
 	FOREIGN KEY (post_id) REFERENCES Media (post_id));
 
 
-
-CREATE TABLE Takes(
-	location_id INTEGER,
-	transportation CHAR(70),
-	km REAL,
-	PRIMARY KEY (location_id, transportation, km),
-	FOREIGN KEY (location_id) REFERENCES Location(id),
-	FOREIGN KEY (transportation,km) REFERENCES Distance(transportation,km));
 CREATE TABLE Posts(
 	post_id INTEGER,
 	trip_id INTEGER,
@@ -99,41 +71,41 @@ CREATE TABLE Posts(
 	FOREIGN KEY (post_id) REFERENCES Media(post_id),
 	FOREIGN KEY (trip_id) REFERENCES Trip_In(trip_id));
 CREATE TABLE Tags(
-post_id  INTEGER,
-location_id  INTEGER,
-PRIMARY KEY (post_id, location_id),
-FOREIGN KEY (post_id) REFERENCES Media(post_id),
-FOREIGN KEY (location_id) REFERENCES Location(id));
+	post_id  INTEGER,
+	location_id  INTEGER,
+	PRIMARY KEY (post_id, location_id),
+	FOREIGN KEY (post_id) REFERENCES Media(post_id),
+	FOREIGN KEY (location_id) REFERENCES Location(id));
 CREATE TABLE IsAt(
-    	activity_name  CHAR(70),
-    	location_id  INTEGER,
-    	PRIMARY KEY (activity_name, location_id),
-    	FOREIGN KEY (activity_name) REFERENCES Activity(name),
-    	FOREIGN KEY (location_id) REFERENCES Location(id));
+	activity_name  VARCHAR(70),
+	location_id  INTEGER,
+	PRIMARY KEY (activity_name, location_id),
+	FOREIGN KEY (activity_name) REFERENCES Activity(name),
+	FOREIGN KEY (location_id) REFERENCES Location(id));
 CREATE TABLE Includes(
-    	trip_id  INTEGER,
-    	activity_name  CHAR(70),
-    	PRIMARY KEY (trip_id, activity_name),
-    	FOREIGN KEY (trip_id) REFERENCES Trip_In(trip_id),
-    	FOREIGN KEY (activity_name) REFERENCES Activity(name));
+	trip_id  INTEGER,
+	activity_name  VARCHAR(70),
+	PRIMARY KEY (trip_id, activity_name),
+	FOREIGN KEY (trip_id) REFERENCES Trip_In(trip_id),
+	FOREIGN KEY (activity_name) REFERENCES Activity(name));
 CREATE TABLE OperatesAt(
-	restaurant_name CHAR(70),
-    	location_id INTEGER,
-    	PRIMARY KEY (restaurant_name, location_id),
-    	FOREIGN KEY (restaurant_name) REFERENCES Restaurant(name),
-    	FOREIGN KEY (location_id) REFERENCES Location(id));
+	restaurant_name VARCHAR(70),
+	location_id INTEGER,
+	PRIMARY KEY (restaurant_name, location_id),
+	FOREIGN KEY (restaurant_name) REFERENCES Restaurant(name),
+	FOREIGN KEY (location_id) REFERENCES Location(id));
 CREATE TABLE Attraction_In(
-    	attr_name CHAR(70),
-    	location_id INTEGER,
-    	type CHAR(70),
-    	description CHAR(120),
-    	num_dollar_signs INTEGER,
-    	PRIMARY KEY (attr_name, location_id),
-    	FOREIGN KEY (location_id) REFERENCES Location(id));
+	attr_name VARCHAR(70),
+	location_id INTEGER,
+	type VARCHAR(70),
+	description VARCHAR(120),
+	num_dollar_signs INTEGER CHECK (num_dollar_signs IN (0, 1, 2, 3)),
+	PRIMARY KEY (attr_name, location_id),
+	FOREIGN KEY (location_id) REFERENCES Location(id));
 CREATE TABLE Plans(
-	username CHAR(16),
+	username VARCHAR(16),
 	trip_id INTEGER,
 	date DATE,
 	PRIMARY KEY (username, trip_id),
-	FOREIGN KEY (username) REFERENCES Users(username),
-    	FOREIGN KEY (trip_id) REFERENCES Trip_In(trip_id));
+	FOREIGN KEY (username) REFERENCES All_Users(username),
+	FOREIGN KEY (trip_id) REFERENCES Trip_In(trip_id));
