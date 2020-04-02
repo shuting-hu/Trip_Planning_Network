89344c679a84adc1d3096d15f6b65734d1b51dd0
MagY <yangm110615@gmail.com>
2020-04-02 14:39:39 -0700

    media uploads, refactoring

--- advengers/create.php
+++ advengers/create.php
@@ -1,238 +1,266 @@
 <?php
+
+// DONE TODO - handle uppercase/lowercase?
+// DONE TODO - refactor this whole thing lmao
+// DONE TODO - caption for photo 
+
+// TODO - make it work for variable user (cookies?)
+// TODO - editing the form???
+// No lol TODO - allow more inputs?
+// TODO - reload feed on go back
+// TODO - CSS for everything, including CSS for error nothing submitted page
+
 include 'connect.php';
 
-// COMMENT OUT ALL sql errors: FIND ' or die', REPLACE with ';//DEBUG or die'
+$conn = OpenCon();
 
-// TODO sanitize inputs to remove apostrophe for example
-function removeSpecialChars($str) {
+if (!isset($_POST["submit"])) {
+    echo "error, nothing submitted.";
+    exit;
+}
+
+// or die(mysqli_error($conn)); to check sql errors
+
+function sanitizeStr($str) {
     $str = str_replace("'", "\'", $str);
-    $str = str_replace('"', "\"", $str);
+    $str = trim($str);
+    $str = !empty($str) ? "'$str'" : "NULL"; // to allow for ez string concat
     return $str;
 }
 
-$conn = OpenCon();
 // FOR TESTING - FILL IN LATER
 $testuser = "insert into `All_Users`(username, password, name)
     values('test', 'abc', 'joe');";
-$testuserresult = mysqli_query($conn, $testuser);//DEBUG or die(mysqli_error($conn));
+$testuserresult = mysqli_query($conn, $testuser);
 
 
 $author = 'test';
 $date = date("Y-m-d");
 
+$title = sanitizeStr($_POST['title']);
+$desc = sanitizeStr($_POST['desc']);
 
-$title = $_POST['title'];
-$desc = $_POST['desc'];
-$desc = !empty($desc) ? "'$desc'" : "NULL";
+$city = sanitizeStr($_POST['city']);
+$province = sanitizeStr($_POST['province']);
+$country = sanitizeStr($_POST['country']);
 
-$city = $_POST['city'];
-$province = $_POST['province'];
-$country = $_POST['country'];
-
-// gets stuck when i use this function
-// $title = removeSpecialChars($_POST['title']);
-// $desc = removeSpecialChars($_POST['desc']);
-// $city = removeSpecialChars($_POST['city']);
-// $province = removeSpecialChars($_POST['province']);
-// $country = removeSpecialChars($_POST['country']);
 $duration = $_POST['duration'];
 
 
 // uses auto-increment so dont need to specify pk val
-$province = !empty($province) ? "'$province'" : "NULL";
-$addloc = "insert into `Location`(country, province, city)
-    values('$country', $province, '$city');";
-$addlocresult = mysqli_query($conn, $addloc);//DEBUG or die(mysqli_error($conn));
+$addloc = "insert into `Location`(country, province, city) values($country, $province, $city);";
+$addlocresult = mysqli_query($conn, $addloc);
 
-$loc_id = mysqli_query($conn, "select id from location where country='$country' and city='$city'");//DEBUG or die(mysqli_error($conn));
+$loc_id = mysqli_query($conn, "select id from location where lower(country)=lower($country) and lower(city)=lower($city)") or die(mysqli_error($conn));
 $loc_id = ($loc_id->fetch_assoc())['id'];
 
 
 $addtrip = "insert into `Trip_In`(title, location_id, duration, description)
-    values('$title', $loc_id, '$duration', $desc);";
-$addtripresult = mysqli_query($conn, $addtrip);//DEBUG or die(mysqli_error($conn));
+    values($title, $loc_id, '$duration', $desc);";
+$addtripresult = mysqli_query($conn, $addtrip);
 
 $tripid = mysqli_insert_id($conn);
 
+
 $addplans = "insert into Plans(username, trip_id)
     values('$author', $tripid)";
-$addplansresult = mysqli_query($conn, $addplans);//DEBUG or die(mysqli_error($conn));
+$addplansresult = mysqli_query($conn, $addplans);
 
 
-$attrname0 = $_POST['attrname0'];
-if ($attrname0 != "") {
-    $attrnumds0 = $_POST['attrnumds0'];
-    $attrtype0 = $_POST['attrtype0'];
-    $attrdesc0 = $_POST['attrdesc0'];
+function addAttr($dbconn, $i, $loc_idx, $tripidx) {
+    $attrname = sanitizeStr($_POST["attrname$i"]);
+    if ($attrname !== "NULL") {
+        $attrnumds = $_POST["attrnumds$i"];
+        $attrtype = sanitizeStr($_POST["attrtype$i"]);
+        $attrdesc = sanitizeStr($_POST["attrdesc$i"]);
 
-    $attrtype0 = !empty($attrtype0) ? "'$attrtype0'" : "NULL";
-    $attrdesc0 = !empty($attrdesc0) ? "'$attrdesc0'" : "NULL";
+        $addAttrQ = mysqli_query($dbconn,
+        "insert into Attraction_In(attr_name, location_id, type, description, num_dollar_signs)
+        values ($attrname, $loc_idx, $attrtype, $attrdesc, $attrnumds)");
+        
+        $inclAttrQ = mysqli_query($dbconn, 
+        "insert into IncludesAttraction(trip_id, attr_name, location_id) values ($tripidx, $attrname, $loc_idx)");
+    }
+}
 
-    $addattr0 = mysqli_query($conn,
-    "insert into Attraction_In(attr_name, location_id, type, description, num_dollar_signs)
-    values ('$attrname0', $loc_id, $attrtype0, $attrdesc0, $attrnumds0)");//DEBUG or die(mysqli_error($conn));
+
+
+function addAct($dbconn, $i, $loc_idx, $tripidx) {
+    $actname = sanitizeStr($_POST["actname$i"]);
+    if ($actname !== "NULL") {
+        $actnumds = $_POST["actnumds$i"];
+        $acttype = sanitizeStr($_POST["acttype$i"]);
+        $actdesc = sanitizeStr($_POST["actdesc$i"]);
+
+        $addActQ = mysqli_query($dbconn,
+        "insert into Activity(name, type, num_dollar_signs, description)
+        values ($actname, $acttype, $actnumds, $actdesc)");
+
+        $addActLocQ = mysqli_query($dbconn,
+        "insert into IsAt(activity_name, location_id)
+        values ($actname, $loc_idx)");
+
+        $inclActQ = mysqli_query($dbconn, 
+        "insert into IncludesActivity(trip_id, activity_name) values($tripidx, $actname)");
+    }
+}
+
+function addRest($dbconn, $i, $loc_idx, $tripidx) {
+    $restname = sanitizeStr($_POST["restname$i"]);
+    if ($restname !== "NULL") {
+        $restnumds = $_POST["restnumds$i"];
+        $resttype = sanitizeStr($_POST["resttype$i"]);
+
+        $addRestQ = mysqli_query($dbconn,
+        "insert into Restaurant(name, cuisine_type, num_dollar_signs)
+        values ($restname, $resttype, $restnumds)");
+        $addRestLocQ = mysqli_query($dbconn,
+        "insert into OperatesAt(restaurant_name, location_id)
+        values ($restname, $loc_idx)");
+
+        $inclRestQ = mysqli_query($dbconn, 
+        "insert into IncludesRestaurant(trip_id, restaurant_name) values($tripidx, $restname)");
+    }
+}
+
+
+
+
+// 1 = text, 2 = photo, 3 = video
+function addYT($yt0, $dbconn, $date0, $tripid0, $loc_id0) {
+    if ($yt0 != "") {
+        $addmedia0 = mysqli_query($dbconn, "insert into media(date, type) values ('$date0', 3)");
+        $mediaid0 = mysqli_insert_id($dbconn);
+        $addyt0 = mysqli_query($dbconn, "insert into video(post_id, url) values($mediaid0, '$yt0')");
+        $addposts0 = mysqli_query($dbconn, "insert into posts(post_id, trip_id) values($mediaid0, $tripid0)");
+        $addtags0 = mysqli_query($dbconn, "insert into tags(post_id, location_id) values($mediaid0, $loc_id0)");
+    }
+}
+
+for ($j = 0; $j < 3; $j++) {
+    addAttr($conn, $j, $loc_id, $tripid);
+    addAct($conn, $j, $loc_id, $tripid);
+    addRest($conn, $j, $loc_id, $tripid);
+    addYT($_POST["yt$j"], $conn, $date, $tripid, $loc_id);
+}
+
+// addYT($_POST['yt0'], $conn, $date, $tripid, $loc_id);
+// addYT($_POST['yt1'], $conn, $date, $tripid, $loc_id);
+// addYT($_POST['yt2'], $conn, $date, $tripid, $loc_id);
+
+function addTxt($txt0, $dbconn, $date0, $tripid0, $loc_id0) {
+    if ($txt0 != "") {
+        //$txt0 = htmlspecialchars($txt0);
+        $txt0 = sanitizeStr($txt0);
+        echo $txt0;
+        $addmedia0 = mysqli_query($dbconn, "insert into media(date, type) values ('$date0', 1)") or die(mysqli_error($dbconn));
+        $mediaid0 = mysqli_insert_id($dbconn);
+        $addtxt0 = mysqli_query($dbconn, "insert into text(post_id, words) values($mediaid0, $txt0)") or die(mysqli_error($dbconn));
+        $addposts0 = mysqli_query($dbconn, "insert into posts(post_id, trip_id) values($mediaid0, $tripid0)") or die(mysqli_error($dbconn));
+        $addtags0 = mysqli_query($dbconn, "insert into tags(post_id, location_id) values($mediaid0, $loc_id0)") or die(mysqli_error($dbconn));
+    }
+}
+
+addTxt($_POST['text0'], $conn, $date, $tripid, $loc_id);
+addTxt($_POST['text1'], $conn, $date, $tripid, $loc_id);
+
+
+$target_dir = "uploads/$author/";
+// count files by shotsy - https://stackoverflow.com/questions/37363231/how-to-count-number-of-uploaded-files-in-php
+// for loop -  https://makitweb.com/multiple-files-upload-at-once-with-php/
+// $countfiles = count(array_filter($_FILES['fileToUpload']['name']));
+// echo "aaaa";
+// echo $countfiles;
+// echo "aaaa";
+$countfiles = 3;
+
+for ($i = 0; $i<$countfiles; $i++) {
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][$i]);
+    echo nl2br("TARGET IS: $target_file\n");
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     
-    $inclAttr0 = mysqli_query($conn, 
-    "insert into IncludesAttraction(trip_id, attr_name, location_id) values ($tripid, '$attrname0', $loc_id)");
+    // Check if image file is a actual image or fake image
+    if (empty($_FILES["fileToUpload"]["tmp_name"][$i])) {
+        echo "exiting, no image provided";
+        continue;
+    }
 
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"][$i]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+        continue;
+    }
+
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+        continue;
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        $imgname = basename($_FILES["fileToUpload"]["name"][$i]);
+        echo nl2br("\n\nSorry, file $imgname already exists. Please rename the file and try again.\n\n");
+        $uploadOk = 0;
+        continue;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"][$i]). " has been uploaded.";
+            $addMedia = mysqli_query($conn, "insert into media (date, type) values ('$date', 2)") or die(mysqli_error($conn));
+            $mediaid = mysqli_insert_id($conn);
+            $cap = $_POST["caption$i"];
+            $cap = sanitizeStr($cap);
+            echo $cap;
+            $addPhoto = mysqli_query($conn, "insert into photo(post_id, caption, file_path) values($mediaid, $cap, '$target_file')") or die(mysqli_error($conn));
+            $addposts0 = mysqli_query($conn, "insert into posts(post_id, trip_id) values($mediaid, $tripid)") or die(mysqli_error($conn));
+            $addtags0 = mysqli_query($conn, "insert into tags(post_id, location_id) values($mediaid, $loc_id)") or die(mysqli_error($conn));
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
 }
-
-$attrname1 = $_POST['attrname1'];
-if ($attrname1 != "") {
-    $attrnumds1 = $_POST['attrnumds1'];
-    $attrtype1 = $_POST['attrtype1'];
-    $attrdesc1 = $_POST['attrdesc1'];
-    
-    $attrtype1 = !empty($attrtype1) ? "'$attrtype1'" : "NULL";
-    $attrdesc1 = !empty($attrdesc1) ? "'$attrdesc1'" : "NULL";
-
-    $addattr1 = mysqli_query($conn,
-    "insert into Attraction_In(attr_name, location_id, type, description, num_dollar_signs)
-    values ('$attrname1', $loc_id, $attrtype1, $attrdesc1, $attrnumds1)");//DEBUG or die(mysqli_error($conn));
-
-    $inclAttr1 = mysqli_query($conn, 
-    "insert into IncludesAttraction(trip_id, attr_name, location_id) values($tripid, '$attrname1', $loc_id)");
-}
-
-$attrname2 = $_POST['attrname2'];
-if ($attrname2 != "") {
-    $attrnumds2 = $_POST['attrnumds2'];
-    $attrtype2 = $_POST['attrtype2'];
-    $attrdesc2 = $_POST['attrdesc2'];
-
-    $attrtype2 = !empty($attrtype2) ? "'$attrtype2'" : "NULL";
-    $attrdesc2 = !empty($attrdesc2) ? "'$attrdesc2'" : "NULL";
-
-    $addattr2 = mysqli_query($conn,
-    "insert into Attraction_In(attr_name, location_id, type, description, num_dollar_signs)
-    values ('$attrname2', $loc_id, $attrtype2, $attrdesc2, $attrnumds2)");//DEBUG or die(mysqli_error($conn));
-
-    $inclAttr2 = mysqli_query($conn, 
-    "insert into IncludesAttraction(trip_id, attr_name, location_id) values($tripid, '$attrname2', $loc_id)");
-}
-
-$actname0 = $_POST['actname0'];
-if ($actname0 != "") {
-    $actnumds0 = $_POST['actnumds0'];
-    $acttype0 = $_POST['acttype0'];
-    $actdesc0 = $_POST['actdesc0'];
-    
-    $acttype0 = !empty($acttype0) ? "'$acttype0'" : "NULL";
-    $actdesc0 = !empty($actdesc0) ? "'$actdesc0'" : "NULL";
-
-    $addact0 = mysqli_query($conn,
-    "insert into Activity(name, type, num_dollar_signs, description)
-    values ('$actname0', $acttype0, $actnumds0, $actdesc0)");//DEBUG or die(mysqli_error($conn));
-
-    $addactloc0 = mysqli_query($conn,
-    "insert into IsAt(activity_name, location_id)
-    values ('$actname0', $loc_id)");//DEBUG or die(mysqli_error($conn));
-
-    $inclAct0 = mysqli_query($conn, 
-    "insert into IncludesActivity(trip_id, activity_name) values($tripid, '$actname0')");
-}
-
-$actname1 = $_POST['actname1'];
-if ($actname1 != "") {
-    $actnumds1 = $_POST['actnumds1'];
-    $acttype1 = $_POST['acttype1'];
-    $actdesc1 = $_POST['actdesc1'];
-
-    $acttype1 = !empty($acttype1) ? "'$acttype1'" : "NULL";
-    $actdesc1 = !empty($actdesc1) ? "'$actdesc1'" : "NULL";
-
-    $addact1 = mysqli_query($conn,
-    "insert into Activity(name, type, num_dollar_signs, description)
-    values ('$actname1', $acttype1, $actnumds1, $actdesc1)");//DEBUG or die(mysqli_error($conn));
-
-    $addactloc1 = mysqli_query($conn,
-    "insert into IsAt(activity_name, location_id)
-    values ('$actname1', $loc_id)");//DEBUG or die(mysqli_error($conn));
-
-    $inclAct1 = mysqli_query($conn, 
-    "insert into IncludesActivity(trip_id, activity_name) values($tripid, '$actname1')");
-}
-
-$actname2 = $_POST['actname2'];
-if ($actname2 != "") {
-    $actnumds2 = $_POST['actnumds2'];
-    $acttype2 = $_POST['acttype2'];
-    $actdesc2 = $_POST['actdesc2'];
-
-    $acttype2 = !empty($acttype2) ? "'$acttype2'" : "NULL";
-    $actdesc2 = !empty($actdesc2) ? "'$actdesc2'" : "NULL";
-
-    $addact2 = mysqli_query($conn,
-    "insert into Activity(name, type, num_dollar_signs, description)
-    values ('$actname2', $acttype2, $actnumds2, $actdesc2)");//DEBUG or die(mysqli_error($conn));
-
-    $addactloc2 = mysqli_query($conn,
-    "insert into IsAt(activity_name, location_id)
-    values ('$actname2', $loc_id)");//DEBUG or die(mysqli_error($conn));
-
-    $inclAct2 = mysqli_query($conn, 
-    "insert into IncludesActivity(trip_id, activity_name) values($tripid, '$actname2')");
-}
-
-$restname0 = $_POST['restname0'];
-if ($restname0 != "") {
-    $restnumds0 = $_POST['restnumds0'];
-    $resttype0 = $_POST['resttype0'];
-    
-    $resttype0 = !empty($resttype0) ? "'$resttype0'" : "NULL";
-
-    $addrest0 = mysqli_query($conn,
-    "insert into Restaurant(name, cuisine_type, num_dollar_signs)
-    values ('$restname0', $resttype0, $restnumds0)");//DEBUG or die(mysqli_error($conn));
-    $addrestloc0 = mysqli_query($conn,
-    "insert into OperatesAt(restaurant_name, location_id)
-    values ('$restname0', $loc_id)");//DEBUG or die(mysqli_error($conn));
-
-    $inclRest0 = mysqli_query($conn, 
-    "insert into IncludesRestaurant(trip_id, restaurant_name) values($tripid, '$restname0')");
-}
-
-$restname1 = $_POST['restname1'];
-if ($restname1 != "") {
-    $restnumds1 = $_POST['restnumds1'];
-    $resttype1 = $_POST['resttype1'];
-
-    $resttype1 = !empty($resttype1) ? "'$resttype1'" : "NULL";
-
-    $addrest1 = mysqli_query($conn,
-    "insert into Restaurant(name, cuisine_type, num_dollar_signs)
-    values ('$restname1', $resttype1, $restnumds1)");//DEBUG or die(mysqli_error($conn));
-    $addrestloc1 = mysqli_query($conn,
-    "insert into OperatesAt(restaurant_name, location_id)
-    values ('$restname1', $loc_id)");//DEBUG or die(mysqli_error($conn));
-
-    $inclRest1 = mysqli_query($conn, 
-    "insert into IncludesRestaurant(trip_id, restaurant_name) values($tripid, '$restname1')");
-}
-
-echo nl2br("things probably worked??? but if you wanna check one by one, uncomment the 'or die' mysqli error handling lines\n\n");
-echo nl2br("still left to do: media inputs\n\n");
+echo nl2br("\n\n hello world \n\n");
 
 mysqli_close($conn);
 
 /*
 use this to check output
 
-SELECT P.username, T.trip_id, T.title, IR.restaurant_name, R.cuisine_type, R.num_dollar_signs
+
+SELECT P.username, T.trip_id, T.location_id, T.title, IR.restaurant_name, R.cuisine_type, R.num_dollar_signs
 FROM Plans P, Trip_In T, IncludesRestaurant IR, Restaurant R
 WHERE P.trip_id = T.trip_id AND IR.trip_id = T.trip_id AND IR.restaurant_name = R.name;
 
-SELECT P.username, T.trip_id, T.title, I.attr_name, A.type, A.num_dollar_signs
+SELECT P.username, T.trip_id, T.location_id, T.title, I.attr_name, A.type, A.num_dollar_signs
 FROM Plans P, Trip_In T, IncludesAttraction I, Attraction_In A
 WHERE P.trip_id = T.trip_id AND I.trip_id = T.trip_id AND A.attr_name = I.attr_name AND A.location_id = I.location_id;
  
-SELECT P.username, T.trip_id, T.title, IA.activity_name, A.type, A.num_dollar_signs
+SELECT P.username, T.trip_id, T.location_id, T.title, IA.activity_name, A.type, A.num_dollar_signs
 FROM Plans P, Trip_In T, IncludesActivity IA, Activity A
 WHERE P.trip_id = T.trip_id AND IA.trip_id = T.trip_id AND IA.activity_name = A.name;
 
 SELECT * FROM Activity;
 SELECT * FROM Attraction_In;
 SELECT * FROM Restaurant;
+SELECT * FROM Location;
+
+SELECT * FROM Video;
+SELECT * FROM Text;
 
 */
+
+
 ?>
