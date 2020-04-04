d5299d56ad06ed6ce9e65505d5d960ad5f56e4c3
ruolin97 <r6u2b@ugrad.cs.ubc.ca>
2020-04-03 23:30:42 -0700

    improvement on search, video display, ui

--- advengers/searchbar-parser.php
+++ advengers/searchbar-parser.php
@@ -7,19 +7,29 @@ if (empty($query)) {
 
 $query = str_replace(',', ' ', $query);
 $words = explode(' ', $query);
+$wordsStr = implode("', '", $words);
 
 include'connect.php';
 $conn = OpenCon();
-
-// reset view
-$sql = "CREATE OR REPLACE VIEW Subposts AS SELECT * FROM Trip_In WHERE trip_id = -1";
+$sql = "CREATE OR REPLACE VIEW Subposts AS SELECT * FROM Trip_In WHERE trip_id IN (SELECT trip_id FROM Plans WHERE username IN ('$wordsStr') UNION SELECT trip_id FROM Trip_In WHERE location_Id IN (SELECT id FROM Location WHERE country IN ('$wordsStr') UNION SELECT id FROM Location WHERE province IN ('$wordsStr') UNION SELECT id FROM Location WHERE city IN ('$wordsStr')))";
 $rsResult = mysqli_query($conn, $sql) or die(mysqli_error($conn));
-foreach ($words as $word) {
-    /*$sql = "CREATE OR REPLACE VIEW Temp AS SELECT * FROM Subposts";
-    $rsResult = mysqli_query($conn, $sql) or die(mysqli_error($conn));*/
-    $sql = "CREATE OR REPLACE VIEW Subposts AS (/*SELECT * FROM Temp UNION*/ SELECT * FROM Trip_In WHERE trip_id IN (SELECT trip_id FROM Plans WHERE username = '$word' UNION SELECT trip_id FROM Trip_In WHERE location_Id IN (SELECT id FROM Location WHERE country = '$word' OR province = '$word' OR city = '$word')))";
-    $rsResult = mysqli_query($conn, $sql) or die(mysqli_error($conn));
-}
 
 header("location: index.php?search=true");
+
+/*$sql = "CREATE VIEW Subposts
+AS
+SELECT *
+FROM Trip_In T
+WHERE T.trip_id IN
+	((SELECT P.trip_id
+	FROM Plans
+	WHERE P.username = '$word')
+	UNION
+	(SELECT T2.trip_id
+	FROM Trip_In T2
+	WHERE T2.location_id IN
+		(SELECT id FROM Location
+		WHERE country = '$word'
+		OR province = '$word'
+		OR city = '$word')));"*/
 ?>
