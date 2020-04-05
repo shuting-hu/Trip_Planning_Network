3e988b7803f7cc0e03bd7289beb9591014a5f271
MagY <yangm110615@gmail.com>
2020-04-04 19:03:24 -0700

    create user dir on register

--- advengers/register.php
+++ advengers/register.php
@@ -11,13 +11,23 @@ $updateRegUsers = "INSERT INTO Regular_User (username) VALUES ('$username')";
 
 $duplicate = mysqli_query($conn, $checkUsername) or die(mysqli_error($conn));
 if (mysqli_num_rows($duplicate) > 0) {
-	echo "<script> alert('Sorry this username is already taken!');parent.location.href='register.html'; </script>";
+	echo "<script> alert('Sorry, this username is already taken!');parent.location.href='register.html'; </script>";
 } else {
 	// update user tables
 	mysqli_query($conn, $updateAllUsers) or die(mysqli_error($conn));
 	mysqli_query($conn, $updateRegUsers) or die(mysqli_error($conn));
-	// back to login page, should we pop up "successful!" message before that?
+	
+	// create a directory for the new user to store their image uploads
+	// https://stackoverflow.com/questions/3997641/why-cant-php-create-a-directory-with-777-permissions
+	$user_posts_path = "./images/posts/" . "$username";
+	$oldmask = umask(0);
+	mkdir($user_posts_path, 0777);
+	umask($oldmask);
+	
+	// back to login page, should we pop up "successful!" message before that? <- yes that would be lovely!
 	header("location: login.html");
+	// however putting echo's before a header redirect won't work, so might need something like this instead:
+		// <script> window.location = "login.html" </script>
 }	
 ?>
 
